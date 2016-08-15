9a7ffe3340f9a23dea2f8ebfbce825bfb07b8763
Justin Lazarski <blackhawks189@gmail.com>
2016-08-14 22:28:52 -0500

    Initial Commit

---
+++ coreDataTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  coreDataTutorial
+//
+//  Created by Justin Lazarski on 8/14/16.
+//  Copyright © 2016 Justin Lazarski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
